388123e8d244466c2b3dfe9c2ab44e43b9b95592
Daniel Reyes <128719578+thanyeal@users.noreply.github.com>
2024-06-11 02:00:40 +0800

    z

--- _app/settings.py
+++ _app/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [BASE_DIR / '_shared/static']
+STATICFILES_DIRS = [BASE_DIR / 'accounts/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
