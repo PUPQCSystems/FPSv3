5a8a10eb09d152fbea875ea12d770ab54b0952db
Daniel Reyes <128719578+thanyeal@users.noreply.github.com>
2024-06-10 14:36:10 +0800

    Update settings.py

--- _app/settings.py
+++ _app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-vk#tvweph+q8z7qi8d(zdnttf1sp9p23ly2cpb%h)q6w74%n1%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
