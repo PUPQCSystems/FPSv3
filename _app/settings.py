303759e21d4d8ae221289e3b8f81a7f6da2f7564
Daniel Reyes <128719578+thanyeal@users.noreply.github.com>
2024-06-11 01:44:12 +0800

    sq:123

--- _app/settings.py
+++ _app/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = "/_shared/static/"
 STATICFILES_DIRS = [BASE_DIR / '_shared/static']
 
 # Default primary key field type
