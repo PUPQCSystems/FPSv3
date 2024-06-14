86244e03c24818112df1fd8000df87a0ca267ba1
Daniel Reyes <128719578+thanyeal@users.noreply.github.com>
2024-06-14 23:49:52 +0800

    ci: revert debug status

--- _app/settings.py
+++ _app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vk#tvweph+q8z7qi8d(zdnttf1sp9p23ly2cpb%h)q6w74%n1%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
